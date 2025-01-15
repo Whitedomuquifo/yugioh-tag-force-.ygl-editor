8da19b04b68beade53905039db4660624145d833
White Do Muquifo Plays <167207012+Whitedomuquifo@users.noreply.github.com>
2025-01-15 10:17:02 -0300

    Delete cards.h

--- cards.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <unordered_map>
-#include "filehandling.h"
-
-void LoadCards(std::unordered_map<Short,std::string> &ID_to_name, std::unordered_map<std::string,Short> &name_to_ID);
